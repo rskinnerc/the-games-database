57fa3c6d96b40d640ae8326c48aa2aa6bcc7e5a2
Ronald <rjskinnerc@unal.edu.co>
2022-06-21 15:05:56 -0500

    Style the home cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -29,14 +29,17 @@ const Home = () => {
         <input placeholder="Filter by genre" className="bg-marino-50 rounded-md p-3 border-0 outline-0 ring-2 focus:ring-marino-400 ring-marino-200 text-marino-600 font-bold" type="text" value={filter} onChange={(e) => setFilter(e.target.value)} />
       </div>
       <div className="font-lato text-sm bg-marino-600 text-white p-1">STATS BY GENRE</div>
-      <ul>
+      <ul className="flex flex-row flex-wrap">
         {genres && genres.map((genre) => (
-          <li key={genre.id} className="even:bg-marino-500 odd:bg-marino-700">
+          <li key={genre.id} className="odd:bg-marino-500 h-52 even:bg-marino-700 basis-1/2 relative">
             <Link to={`/genre/${genre.slug}`}>
-              <img className="grayscale mix-blend-luminosity hover:grayscale-0 hover:mix-blend-normal" src={genre.image_background} alt={genre.name} />
-              <h2>{genre.name}</h2>
-              <h3>
-                Games Count:
+              <i className="bi bi-arrow-right-circle absolute top-0.5 right-1 z-30 font-bold text-white" />
+              <img className="h-3/4 grayscale mix-blend-luminosity hover:grayscale-0 hover:mix-blend-normal" src={genre.image_background} alt={genre.name} />
+              <h2 className="font-lato font-semibold text-white px-1 text-right">{genre.name}</h2>
+              <h3 className="text-sm text-white font-lato px-1 text-right">
+                <span className="text-marino-200">
+                  Games Count:
+                </span>
                 {' '}
                 {genre.games_count}
               </h3>
