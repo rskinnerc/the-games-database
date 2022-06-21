ee01c91e7d33d5885091edf46ee212303ae285bf
Ronald <rjskinnerc@unal.edu.co>
2022-06-21 12:11:21 -0500

    Update base color and refactor genres page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,15 +15,24 @@ const Home = () => {
 
   return (
     <div>
-      <h1>Genres</h1>
-      <input type="text" value={filter} onChange={(e) => setFilter(e.target.value)} />
+      <div className="bg-gradient-to-r from-marino-600 to-marino-600 via-marino-300">
+        <h1>The largest video games database.</h1>
+        <p>
+          More than 350.000 games for 50 platforms including mobiles. Start now filtering by genre!
+        </p>
+        <input type="text" value={filter} onChange={(e) => setFilter(e.target.value)} />
+      </div>
       <ul>
         {genres && genres.map((genre) => (
-          <li key={genre.id}>
+          <li key={genre.id} className="even:bg-marino-500 odd:bg-marino-700">
             <Link to={`/genre/${genre.slug}`}>
-              {genre.name}
-              {' '}
-              {genre.games_count}
+              <img className="grayscale mix-blend-multiply hover:grayscale-0 hover:mix-blend-normal" src={genre.image_background} alt={genre.name} />
+              <h2>{genre.name}</h2>
+              <h3>
+                Games Count:
+                {' '}
+                {genre.games_count}
+              </h3>
             </Link>
           </li>
         ))}
