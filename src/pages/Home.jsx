813077cc53bf65c46c2fa424b502b89e209a9ec0
Ronald <rjskinnerc@unal.edu.co>
2022-06-20 20:28:10 -0500

    Add fetching genres async thunk

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,15 @@
-const Home = () => (<div>Home Page</div>);
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchAllGenres } from '../redux/categories/categoriesSlice';
+
+const Home = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchAllGenres());
+  });
+
+  return (<div>Home Page</div>);
+};
 
 export default Home;
--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -1,14 +1,30 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   categories: [],
+  count: 0,
 };
+
+export const fetchAllGenres = createAsyncThunk(
+  'categories/FetchAllGenres',
+  async () => {
+    const res = await fetch('https://api.rawg.io/api/genres?key=f701489903124362ba098939ed7babd7');
+    return res.json();
+  },
+);
 
 export const categoriesSlice = createSlice({
   name: 'categories',
   initialState,
   reducers: {
 
+  },
+  extraReducers: (builder) => {
+    /* eslint-disable no-param-reassign */
+    builder.addCase(fetchAllGenres.fulfilled, (state, action) => {
+      state.count = action.payload.count;
+      state.categories = action.payload.results;
+    });
   },
 });
 
