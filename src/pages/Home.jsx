0fdf2e7b5deb280aaa2cc0d9b3260bb8248f487b
Ronald <rjskinnerc@unal.edu.co>
2022-06-20 20:36:19 -0500

    Display the list of genres

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,29 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchAllGenres } from '../redux/categories/categoriesSlice';
 
 const Home = () => {
   const dispatch = useDispatch();
+  const genres = useSelector((state) => state.categories.categories);
 
   useEffect(() => {
     dispatch(fetchAllGenres());
-  });
+  }, []);
 
-  return (<div>Home Page</div>);
+  return (
+    <div>
+      <h1>Genres</h1>
+      <ul>
+        {genres && genres.map((genre) => (
+          <li key={genre.id}>
+            {genre.name}
+            {' '}
+            {genre.games_count}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
