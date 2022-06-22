b58e6d593cf56ac7245f646ffa11766f0c1fdefd
Ronald <rjskinnerc@unal.edu.co>
2022-06-22 11:08:38 -0500

    Add test for the home page

---
+++ src/pages/home.test.js
@@ -0,0 +1,58 @@
+import { screen, waitFor, render } from "@testing-library/react";
+import store from '../redux/configureStore'
+import { Provider } from "react-redux"
+import { MemoryRouter } from "react-router-dom"
+import App from "../App"
+import userEvent from "@testing-library/user-event";
+
+describe("The home page functionality", () => {
+  it("should load the genres from the API and list them from Redux store", async () => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={['/']} initialIndex={0}>
+          <App />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    const genreLinks = await screen.findAllByText("Games Count:", { exact: false });
+    expect(genreLinks.length).toBeGreaterThan(0);
+  });
+
+  it("should allow the user to filter the loaded genres", async () => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={['/']} initialIndex={0}>
+          <App />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    const genreLinks = await screen.findAllByText("Games Count:", { exact: false });
+    expect(genreLinks.length).toBeGreaterThan(0);
+
+    const filterInput = await screen.findByPlaceholderText("Filter by genre");
+    userEvent.type(filterInput, "This long string should not bring any result filtering by genre name.");
+
+    const filteredLinks = await waitFor(() => screen.queryAllByText("Games Count:", { exact: false }));
+    expect(filteredLinks.length).toBe(0);
+  });
+
+  it("should navigate to the genre details page when clicking a genre card", async () => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={['/']} initialIndex={0}>
+          <App />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    const genreLinks = await screen.findAllByText("Games Count:", { exact: false });
+    userEvent.click(genreLinks[0]);
+
+    const gamesBreakdownText = await screen.findByText("GAMES BREAKDOWN", { exact: false });
+    const votesText = await screen.findAllByText("votes", { exact: false });
+    expect(gamesBreakdownText).toBeInTheDocument();
+    expect(votesText.length).toBeGreaterThan(0);
+  })
+})
